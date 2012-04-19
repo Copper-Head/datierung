a536efd08d120a7aea3cd6109854d08fca946520
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2012-04-20 00:52:21 +0200

    Solved the problem with erroneous weekday estimation. Added a main() method. Added comments, updated README.

--- README.txt
+++ README.txt
@@ -1,22 +1,58 @@
-*********** READ ME file **********
-April 1st 2012
+*********** README file **********
 
-A potential someday-to-be program for converting roman/medieval dates into 
-conventional formats written by Ilia Alexander Kurenkov.
+N.B.DESCRIPTION
+This is a "baby" date converter program written by Ilia Kurenkov
+(ilia.kurenkov@gmail.com) as an excercise in python. As such it can be 
+considered free software licensed under the GPL.
 
-ilia.kurenkov@gmail.com
+I got the idea for this program while doing a homework assignment for a 
+medieval history class at the University of Konstanz.
+I noticed that we used a series of tables to figure out what the modern date
+format of the dates we encountered in chronics and documents for the course.
+
+These tables can be found at this link (N.B. webpage in German):
+http://www.adfontes.uzh.ch/1001.php
+
+The website will ask for a login, feel free to just use my first and last name
+for the ID and password respectively (both should be capitalized). No personal
+information will be compromised, because you will only have (relatively)
+anonymous access to the website. Once logged in, click on the tab 'Resoursen',
+then on the link 'Datierung Auflösen'.
 
 
-DESCRIPTION
+Seeing as the program was first and foremost an excercise, it only processes
+input dates given in certain formats and does only a limited number of
+operations with them. The only currently acceptable format for a date is the
+following (all numerals should be roman):
+
+dd refdate month year
+
+where refdate stands for nones/ides/calendes type of reference date.
+The years currently covered are 0-2099.
+
+Another thing to keep in mind is that this program accepts a mixture of
+roman-style month names along with English and even German month names. 
+It also only deals with Julian/Gregorian month names, pre-Julian roman dates
+are currently not supported.
+
+Things that I would like to add to this system:
+	- support B.C. dates as well as A.D.
+	- support for different input formats including partial date entries
+	- support for various kinds of roman calendars (pre/post-Numa reform)
+	- support for greek calendar
+	- [ambitious] capability to extract dates from text and convert them
+	
+
+Known Bugs:
+Currently, if we run the find_weekday() method without first running the
+calculate_date() one we will be using the original input values for self.day
+and self.month, which could be different from their actual value, since they
+often get changed as part of the date calculation process.
 At this point in time this is not exactly a program, more a collection of files
 and modules that I plan to combine later on. The purpose of this program [in
 the future] is to find instances of roman/medieval dates in documents and
 convert them into conventional dates.
 
-The program started out merely as an excercise to keep me from forgetting how
-to code. I am placing it on github in order to a) keep track of my changes,
-because I am at a stage where recovery/traceback can be quite tedious and b) to
-get some practice with github in along the way.
 
 SYSTEM REQUIREMENTS
 All of the files in this program were created using Python 2.7.
--- dateconvert.py
+++ dateconvert.py
@@ -1,14 +1,41 @@
-# This file is to convert roman numerals into arabic ones
-import re
+''' Short sample program for converting roman-style dates into conventional
+format created by Ilia Alexander Kurenkov as an excercise in Python. The
+program is free.
+Please keep in mind, that the only currently supported input date format is:
+	DD refdate Month Year
+where refdate stands for nones/ides/calendes and all the numbers are roman
+numerals.
 
+For more information about the program in general, please refer to the 
+README.txt
+
+This particular file contains two classes: Month and DateProcessor that between
+them provide the functionality to convert some date from roman to conventional
+format and also tell what day of the week that date corresponded to.
+'''
+
+import re # used to scanning strings 
+
+''' to start with some constants: names of days of the week... '''
 DAYS = ['Sunday', 'Saturday', 'Friday', 'Thursday', 'Wednesday', 'Tuesday',
 		'Monday'
 		]
 
+''' dictionary of brackets used to find day of week based on sunday letter '''
 BRACKETS = {(1,10):1, (2,3,11):2, (4,7): 3, (8,0):4, (9,12):5, (5,0):6, (6,0):7}
-LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-CENTURIES = {0:0,100:-1,200:-2,300:-3,400:-4,500:-5}
 
+''' list of sunday letters '''
+LETTERS = ['D','C','B','A','G','F','E']
+
+''' dictionary of centuries as keys and index changes as values. for more, see
+narrow_to_century() method 
+'''
+CENTURIES = {0:0, 100:-1, 200:-2, 300:-3, 400:-4, 500:-5, 600:-6,
+		700:0, 800:-1, 900:-2, 1000:-3, 1100:-4, 1200:-5, 1300:-6,
+		1400:0, 1500:-1, 1600:-2, 1700:-3, 1800:-4, 1900:-5, 2000:-6
+		}
+
+''' class for months '''
 class Month:
 	def __init__(self, regex, month_num, d=31):
 		self.name = re.compile(regex)
@@ -20,10 +47,13 @@ class Month:
 		self.check_lateness(month_num)
 
 	def check_lateness(self, n):
+		# check if month has later nones and ides dates
 		if n in [3,5,7,10]:
 			self.Nones, self.Ides = 7, 15
 	
 	def find_date(self, n, flag=None):
+		# return date of month 
+		# takes arabic number as argument along with flags described below
 		if flag == 'c':
 			return self.Calends - n+1
 		elif flag == 'n':
@@ -33,10 +63,10 @@ class Month:
 		elif flag == 'e':
 			return self.lastDay - n
 		else:
-			print 'Invalid flag, please use c, n or i'
+			print 'Invalid flag, please use c, n, e or i'
 			return None
 
-# create instances of this object for months, kalendes, nones and ides
+''' create instances of months '''
 jan = Month('[Jj]an(uar)?y?', 1)
 feb = Month('[Ff]eb(ruar)?y?', 2, d=28)
 mar = Month('[Mm]ar(ch)?', 3)
@@ -50,15 +80,20 @@ okt = Month('[Oo][ck]t(ober)?', 10)
 nov = Month('[Nn]ov(ember)?', 11, d=30)
 dec = Month('[Dd]ec(ember)?', 12)
 
+''' create regex pattern objects for calendes, nones and ides '''
 calendes = re.compile('[KkCc]alende?s')
 nones = re.compile('[Nn]ones')
 ides = re.compile('[Ii]des')
 
-# put months into a list that we can loop over later
+''' put months into a list that we can loop over later '''
 MONTHS = [jan, feb, mar, apr, may, jun, jul, aug, sept, okt, nov, dec]
 
+''' Define class for processing dates. I found this necessary so as not to
+initiate and pass around certain variables everytime certain methods are run
+'''
 class DateProcessor:
 	def __init__(self, in_date):
+		''' class constructor '''
 		self.one = re.compile('[iI]')
 		self.four = re.compile('([iI][Vv])')
 		self.five = re.compile('[vV]')
@@ -76,17 +111,17 @@ class DateProcessor:
 		self.year = self.convert_to_arab(in_date[3])
 
 	def convert_to_arab(self, roman):
-# We define our output variable
 		arab = 0
-# Then all of the numerals of interest
-# Start by finding all the occurences of numbers 4 and 9
+		''' start by finding occurences of 4 and 9 '''
 		if self.four.search(roman):
 			arab += 4
-			roman = self.four.sub('', roman)
+			roman = self.four.sub('', roman) # deleted to avoid counting it again
 		if self.nine.search(roman):
 			arab += 9
 			roman = self.nine.sub('', roman)
-# Then count all of the occurences of the other characters
+			''' Then we count all the occurences of the other characters, since
+			they do not overlap.
+			'''
 		ones = len(self.one.findall(roman))
 		fives = len(self.five.findall(roman))*5
 		tens = len(self.ten.findall(roman))*10
@@ -94,14 +129,21 @@ class DateProcessor:
 		hundreds = len(self.hundred.findall(roman))*100
 		five_hundreds = len(self.five_hundred.findall(roman))*500
 		thousands = len(self.thousand.findall(roman))*1000
-# Sum up all of the counts and print the results
+
+		''' We then sum up all the counts and return that sum '''
 		arab = arab + ones + fives + fifties + tens + hundreds + five_hundreds + thousands
 		return arab
 	
 	def calculate_date(self):
-		for m in MONTHS:
+		''' Argumentless method modifies self.day and self.month to reflect the
+		conventional day and month information.
+		'''
+		for m in MONTHS: #loop over month objects to see which one matches 
 			if m.name.search(self.month):
 				outputMonth = m
+
+		'''Having found a prelim month, we try to identify the reference date.
+		'''
 		if nones.search(self.cni):
 			self.day = outputMonth.find_date(self.day,'n')
 
@@ -111,17 +153,22 @@ class DateProcessor:
 		elif calendes.search(self.cni):
 			self.day = outputMonth.find_date(self.day,'c')
 
+		'''If value of day negative, go back on month and count again. '''
 		if self.day < 0:
 			outputMonth = MONTHS[MONTHS.index(outputMonth)-1]
 			self.day = outputMonth.find_date(abs(self.day),'e')
-		
+		''' once all is set, finalize the value of self.month. '''	
 		self.month = outputMonth.num
 
 
 	def narrow_to_century(self):
-		tempCent = {}
+		'''Method to tie a year value with a century and use that along with
+		the year number striped of hundreds/thousands. These values are then
+		used by the find_sunday_letter() method to calculate the appropriate
+		sunday letter(s) for this year. '''
+		tempCent = {} #temporary dict to store centuries that match 1st filter
 		for cent in CENTURIES:
-			if self.year-cent > 0:
+			if self.year-cent >= 0:
 				tempCent[cent] = CENTURIES[cent]
 		for c in tempCent:
 			if self.year-c == min(self.year-cent for cent in tempCent):
@@ -129,15 +176,17 @@ class DateProcessor:
 	
 	def find_sunday_letter(self, years, start=0, first=True, leap=False):
 		if not first:
-# if this is not first run
+			''' if this is not first run '''
 			indx = start + years
 			leap_year = leap
 		else:
-# if first run
+			''' if first run '''
 			indx = start + years + years/4 + 1
 			if not years % 4:
-# if a leap year, counterintuitively phrased since we want 'years % 4' to equal
-# 0, and this is a way in Python to check for this
+				''' If a leap year, counterintuitively phrased to satisfy 
+				python's syntax, since we want 'years % 4' to equal 0, and this 
+				is how Python checks for this.
+				'''
 				leap_year=True
 			else:
 				leap_year = False
@@ -147,46 +196,61 @@ class DateProcessor:
 			else:
 				return LETTERS[indx]
 		except IndexError:
-# If our index value is out of range, we recursively keep decreasing it by
-			# 7 (length of our week)
+			''' If our index value is out of range, we recursively keep 
+			decreasing it by 7 (length of our week)
+			'''
 			return self.find_sunday_letter(indx-7, first=False, leap=leap_year)
 
 	def find_weekday(self):
+		''' Method to determine what day of the week our date was. Since this
+		is a somewhat side-line feature, the method actually returns the
+		weekday instead of modifying instance attributes, as the
+		calculate_date() one does.
+		'''
 		date = self.day
 		month = self.month
 		cent_bracket = self.narrow_to_century()
 		letter = self.find_sunday_letter(cent_bracket[0], start=cent_bracket[1])
-		if len(letter) == 2:
-			if month <= 2:
-				letter = letter[0]
+		if len(letter) == 2: #if we have a leap year 
+			if month <= 2:	#if the month is january or february
+				letter = letter[0] #use first sunday letter 
 			else:
-				letter = letter[1]
+				letter = letter[1]	# use second sunday letter
+		''' Identify bracket number. '''
 		for b in BRACKETS:
 			if month in b:
-				brack = BRACKETS[b]
-		if not brack % 2:
-			firstMember = (brack + 8)/2
+				bracket_num = BRACKETS[b]
+		''' Now find first member of bracket. '''
+		if not bracket_num % 2: #if bracket number is even 
+			firstMember = (bracket_num + 8)/2
 		else:
-			firstMember = (brack + 1)/2
+			firstMember = (bracket_num + 1)/2
+		''' Find how many columns we need to move from first bracket date. '''
 		steps = date - firstMember
 		if steps >= 0:
 			steps = steps % 7
 		else:
 			steps = 7 + steps
-		weekDay = DAYS[LETTERS.index(letter) - steps]
+		L = (LETTERS[4:]+LETTERS[:4])[::-1] #we reshuffle the letter list 
+		weekDay = DAYS[L.index(letter) - steps]
 		return weekDay
 
-
-
 def main():
+	''' Main method to be run if file invoked from command line.
+	Currently takes no arguments.
+	'''
+	''' When creating input, please keep in mind that it must be in the format:
+	DD refdate Month Year
+	with refdate referring to ides/nones/calendes, all nums must be roman nums.
+	'''
 	#input_date = raw_input('Please enter a roman date in the following format:\
 #\nDD ides/calends/nonnes MM YEAR\n')
-	input_date = 'iv calendes march MMXII'
+	#input_date = 'iv calendes march MMXII' # just an example date 
+	input_date = 'iv calendes march MM' # just an example date 
 	DP = DateProcessor(input_date)
 	DP.calculate_date()
 	weekday = DP.find_weekday()
 	print DP.day, DP.month, DP.year, weekday
-	#print DP.day, DP.month, DP.year
 
 if __name__ == '__main__':
 	main()
